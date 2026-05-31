4a105862b38e8c3f4a494a547ba624fd52d8ef43
Sofia Rubachin <102734675+sofiruba@users.noreply.github.com>
2026-05-31 19:43:06 -0300

    Update api.ts

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,3 +1,5 @@
 export const API_URL = "http://192.168.1.47:3000";
-// si estas en celular pones ipconfig getifaddr en0 en consola y esa ip la usas aca, si estas en emulador de android podes usar 10.0.2.2
-// en uade haces lo mismo, con compu podes usar "http://localhost:3000" 
+// si estas en celular pones "ipconfig getifaddr en0" en consola y esa ip la usas aca, 
+// si estas en emulador de android podes usar 10.0.2.2
+// en windows podes usar "ipconfig" y buscar la ip de tu red local, y esa ip la usas aca
+// en uade haces lo mismo, si ves la app de web podes usar "http://localhost:3000" 
