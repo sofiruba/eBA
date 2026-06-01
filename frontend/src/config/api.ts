b60725967df4b97194e0e9a460e8a1fcb6d31829
Sofia Rubachin <102734675+sofiruba@users.noreply.github.com>
2026-06-01 18:25:10 -0300

    gitignore

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
 export const API_URL = "http://192.168.68.103:3000";
+=======
+export const API_URL = "http://172.20.10.14:3000";
+>>>>>>> Stashed changes
 // si estas en celular pones "ipconfig getifaddr en0" en consola y esa ip la usas aca, 
 // si estas en emulador de android podes usar 10.0.2.2
 // en windows podes usar "ipconfig" y buscar la ip de tu red local, y esa ip la usas aca
